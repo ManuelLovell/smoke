2ac7d2a7e22ace77e8c9dfb5df5069d5c8211263
Manuel DeJesus <69539934+ManuelLovell@users.noreply.github.com>
2024-05-09 02:50:07 -0700

    ViteConfigUpdate

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,25 @@
-import path from 'path'
+import path from 'path';
 
 export default {
-    build: {
-        target: 'esnext',
-        rollupOptions: {
-            input: {
-                main: path.resolve(__dirname, 'index.html'),
-                linetool: path.resolve(__dirname, 'pages/line.html'),
-                brushtool: path.resolve(__dirname, 'pages/brush.html'),
-                polygontool: path.resolve(__dirname, 'pages/polygon.html'),
-                processed: path.resolve(__dirname, 'pages/processed.html'),
-                whatsnew: path.resolve(__dirname, 'pages/whatsnew.html')
-            }
-        }
+  build: {
+    target: 'esnext',
+    rollupOptions: {
+      input: {
+        main: path.resolve(__dirname, 'index.html'),
+        linetool: path.resolve(__dirname, 'pages/line.html'),
+        brushtool: path.resolve(__dirname, 'pages/brush.html'),
+        polygontool: path.resolve(__dirname, 'pages/polygon.html'),
+        processed: path.resolve(__dirname, 'pages/processed.html'),
+        whatsnew: path.resolve(__dirname, 'pages/whatsnew.html')
+      }
     }
-}
+  },
+  worker: {
+    // Configure worker options
+    format: 'es', // Output format for worker bundle (ES module)
+    // Additional worker options can be specified here, if needed
+    // For example:
+    // plugins: [], // Vite plugins that apply to the worker bundles
+    // rollupOptions: {}, // Rollup options to build worker bundle
+  },
+};
