2ab04e524c676d72c020215331285a2e92595ca3
Manuel DeJesus <69539934+ManuelLovell@users.noreply.github.com>
2024-05-20 22:18:27 -0700

    PageMissing

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,7 @@ export default {
         main: path.resolve(__dirname, 'index.html'),
         linetool: path.resolve(__dirname, 'pages/line.html'),
         brushtool: path.resolve(__dirname, 'pages/brush.html'),
+        elevationtool: path.resolve(__dirname, 'pages/elevation.html'),
         polygontool: path.resolve(__dirname, 'pages/polygon.html'),
         processed: path.resolve(__dirname, 'pages/processed.html'),
         whatsnew: path.resolve(__dirname, 'pages/whatsnew.html')
