1b89a4c9b2088e2863daf0bfb9a2b7024dd6c041
ManuelLovell <69539934+ManuelLovell@users.noreply.github.com>
2024-10-14 23:10:23 +0000

    Build: (6244680) update: closer trailing fog

--- assets/notice-Dds4Mqr0.js
+++ assets/notice-Dds4Mqr0.js
@@ -1,4 +1,4 @@
-import{O as e,C as n}from"./bsConstants-DnJON3hn.js";const i=document.querySelector("#bs-notice"),l=document.querySelector("#bs-notice-notes");i.innerHTML=`
+import{O as e,C as n}from"./bsConstants-CMRxC4u1.js";const i=document.querySelector("#bs-notice"),l=document.querySelector("#bs-notice-notes");i.innerHTML=`
   <div id="newsContainer">
     <h1>Smoke & Spectre! 3.0</h1>
     Large thank you to the OBR Devs for all of the development that let these changes be possible.
