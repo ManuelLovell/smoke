eb2cf2da537a073c20c7de3601a8308a90c0d6aa
Manuel St Martin <mdjxbl@gmail.com>
2025-12-15 20:15:06 -0800

    fix: hold line state on trim

--- src/tools/visionDoorMode.ts.ts
+++ src/tools/visionDoorMode.ts.ts
@@ -11,15 +11,13 @@ let interaction: [any, any] | [any] | null = null;
 let stopProcess = false;
 let createInteraction = false;
 
-export async function cancelDrawing(): Promise<void>
-{
+export async function cancelDrawing(): Promise<void> {
     newSegment = [];
     targetLine = "";
     await OBR.scene.local.deleteItems([newPlaceholder]);
 }
 
-export async function finishDrawing(oldLine: Curve): Promise<void>
-{
+export async function finishDrawing(oldLine: Curve): Promise<void> {
     const uniqueShape = (oldLine.position.x !== 0 && oldLine.position.y !== 0);
     const originalLine = uniqueShape ? adjustPoints(oldLine.points, oldLine.position, true) : oldLine.points;
     const newLines = SplitLines(originalLine, newSegment[0], newSegment[1], oldLine.rotation, oldLine.position);
@@ -27,47 +25,38 @@ export async function finishDrawing(oldLine: Curve): Promise<void>
     // Build New Line
     const newLinePackage: Curve[] = [];
     const line = buildCurve()
-        .tension(0)
+        .tension(oldLine.style.tension ?? 0)
         .points(newLines.extracted)
-        .strokeColor(InvertColor(BSCACHE.sceneMetadata[`${Constants.EXTENSIONID}/toolColor`] as string ?? Constants.DEFAULTLINECOLOR))
-        .strokeDash(BSCACHE.sceneMetadata[`${Constants.EXTENSIONID}/toolStyle`] as [] ?? Constants.DEFAULTLINESTROKE)
-        .strokeWidth(GetToolWidth())
-        .fillOpacity(0)
-        .fillColor("#000000")
-        .layer(Constants.LINELAYER)
-        .name("Vision Line (Line)")
-        .metadata({
-            [`${Constants.EXTENSIONID}/isVisionLine`]: true,
-            [`${Constants.EXTENSIONID}/doubleSided`]: true,
-            [`${Constants.EXTENSIONID}/blocking`]: true
-        })
-        .closed(false)
-        .visible(false)
-        .locked(true)
+        .strokeColor(InvertColor(oldLine.style.strokeColor ?? Constants.DEFAULTLINECOLOR))
+        .strokeDash(oldLine.style.strokeDash ?? Constants.DEFAULTLINESTROKE)
+        .strokeWidth(oldLine.style.strokeWidth ?? GetToolWidth())
+        .fillOpacity(oldLine.style.fillOpacity ?? 0)
+        .fillColor(oldLine.style.fillColor ?? "#000000")
+        .layer(oldLine.layer ?? Constants.LINELAYER)
+        .name(oldLine.name ?? "Vision Line (Line)")
+        .metadata(oldLine.metadata)
+        .closed(oldLine.style.closed ?? false)
+        .visible(oldLine.visible ?? false)
+        .locked(oldLine.locked ?? true)
         .build();
 
     newLinePackage.push(line);
-    for (const remainder of newLines.remaining)
-    {
+    for (const remainder of newLines.remaining) {
         const remainingLine = buildCurve()
             .tension(0)
             .position(oldLine.position)
-            .strokeColor(BSCACHE.sceneMetadata[`${Constants.EXTENSIONID}/toolColor`] as string ?? Constants.DEFAULTLINECOLOR)
-            .strokeDash(BSCACHE.sceneMetadata[`${Constants.EXTENSIONID}/toolStyle`] as [] ?? Constants.DEFAULTLINESTROKE)
-            .strokeWidth(GetToolWidth())
-            .fillOpacity(0)
+            .strokeColor(oldLine.style.strokeColor ?? Constants.DEFAULTLINECOLOR)
+            .strokeDash(oldLine.style.strokeDash ?? Constants.DEFAULTLINESTROKE)
+            .strokeWidth(oldLine.style.strokeWidth ?? GetToolWidth())
+            .fillOpacity(oldLine.style.fillOpacity ?? 0)
             .rotation(oldLine.rotation)
-            .fillColor("#000000")
-            .layer(Constants.LINELAYER)
-            .name("Vision Line (Line)")
-            .metadata({
-                [`${Constants.EXTENSIONID}/isVisionLine`]: true,
-                [`${Constants.EXTENSIONID}/doubleSided`]: true,
-                [`${Constants.EXTENSIONID}/blocking`]: true
-            })
-            .closed(false)
-            .visible(false)
-            .locked(true)
+            .fillColor(oldLine.style.fillColor ?? "#000000")
+            .layer(oldLine.layer ?? Constants.LINELAYER)
+            .name(oldLine.name ?? "Vision Line (Line)")
+            .metadata(oldLine.metadata)
+            .closed(oldLine.style.closed ?? false)
+            .visible(oldLine.visible ?? false)
+            .locked(oldLine.locked ?? true)
             .build();
 
         remainingLine.points = uniqueShape ? adjustPoints(remainder, oldLine.position, false) : remainder;
@@ -80,58 +69,47 @@ export async function finishDrawing(oldLine: Curve): Promise<void>
     cancelDrawing();
 }
 
-async function onToolClick(_: ToolContext, event: ToolEvent): Promise<void>
-{
+async function onToolClick(_: ToolContext, event: ToolEvent): Promise<void> {
     if (event.transformer) return;
 
     if (!event.target || (event.target.type !== "CURVE") || (targetLine !== "" && targetLine !== event.target.id)) return;
 
     targetLine = event.target.id;
     newSegment.push(event.pointerPosition);
-    if (newSegment.length === 1)
-    {
+    if (newSegment.length === 1) {
         const placeholder = GetPointer();
         placeholder.position = event.pointerPosition;
         newPlaceholder = placeholder.id;
         await OBR.scene.local.addItems([placeholder]);
     }
 
-    if (newSegment.length === 2)
-    {
+    if (newSegment.length === 2) {
         finishDrawing(event.target as Curve);
     }
 }
 
-async function onToolMove(_: ToolContext, event: ToolEvent)
-{
-    if (event.target && event.target.type === "CURVE" && (targetLine === "" || targetLine === event.target.id))
-    {
-        if (!stopProcess && !createInteraction)
-        {
-            if (!interaction)
-            {
+async function onToolMove(_: ToolContext, event: ToolEvent) {
+    if (event.target && event.target.type === "CURVE" && (targetLine === "" || targetLine === event.target.id)) {
+        if (!stopProcess && !createInteraction) {
+            if (!interaction) {
                 createInteraction = true;
                 interaction = await OBR.interaction.startItemInteraction(GetPointer());
             }
             // show circle on line
             const [update] = interaction;
-            update((circle: Pointer) =>
-            {
+            update((circle: Pointer) => {
                 circle.position = event.pointerPosition;
             });
             createInteraction = false;
         }
     }
-    else if (interaction && (!event.target || (event.target && event.target.type !== "CURVE")))
-    {
+    else if (interaction && (!event.target || (event.target && event.target.type !== "CURVE"))) {
         const [_, stop] = interaction;
         stop();
         interaction = null;
 
-        if (!stopProcess)
-        {
-            setTimeout(() =>
-            {
+        if (!stopProcess) {
+            setTimeout(() => {
                 stopProcess = false;
             }, 50);
             stopProcess = true;
@@ -139,16 +117,13 @@ async function onToolMove(_: ToolContext, event: ToolEvent)
     }
 }
 
-function onKeyDown(_: ToolContext, event: KeyEvent)
-{
-    if (event.key == "Escape")
-    {
+function onKeyDown(_: ToolContext, event: KeyEvent) {
+    if (event.key == "Escape") {
         cancelDrawing();
     }
 }
 
-function GetPointer(): Pointer
-{
+function GetPointer(): Pointer {
     const pointerSize = GetToolWidth();
     const pointer = buildPointer()
         .color("red")
@@ -158,8 +133,7 @@ function GetPointer(): Pointer
     return pointer;
 }
 
-function adjustPoints(points: Vector2[], adjustment: Vector2, add: boolean): Vector2[]
-{
+function adjustPoints(points: Vector2[], adjustment: Vector2, add: boolean): Vector2[] {
     return points.map(point => ({
         x: add ? point.x + adjustment.x : point.x - adjustment.x,
         y: add ? point.y + adjustment.y : point.y - adjustment.y
